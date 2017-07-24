98efba7389b13baced32a126c428faa3e998e8b5
ttu94 <ttu94@hotmail.com>
2017-07-24 06:57:52 +0000

    format queen caila. figured out the map in one fell swoop. and the email now can deliver and sort messages into the separate enquiry and feedback inboxes.

--- project/app/views/adminView/adminHomePage.blade.php
+++ project/app/views/adminView/adminHomePage.blade.php
@@ -12,18 +12,17 @@
 @section('content')
 <div class="container-fluid">
     <br>
-    <div style="width:80%; margin: 0 auto">
+    <div style="width:85%; margin: 0 auto">
+        <p class="message">See where your users are using the site from around the world!</p>
         <!--Map widget-->
-        <script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?cl=080808&w=a&t=t&d=t7Ne12ZQ9RbTOqBlI5Lc69T8egnJwx0i5PU98EvOLXM&co=ffffff&cmo=ffe605&cmn=ff0000&ct=808080"></script> 
+        <script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?cl=7790a8&w=a&t=t&d=l4_SQF59n1LMpAYnynWf-2F4_dHGv01wIKqrn-o3LQA&co=efefef&cmo=ad00ff&cmn=ffd600&ct=000000"></script>
     </div>
-    
+    <br>
     <div class="col-md-12 basicFontStyle">
-        <a target="_blank" href="phpliteadmin.php">Database Management</a>
-        <br>
+        <p>Access the database by clicking the following link: <a target="_blank" href="phpliteadmin.php">Database Management</a></p>
         <!--<a href="admin_quiz_editor">Admin Quiz Editor</a>-->
-        <a href={{ route("admin_quiz_editor", array("id" => Auth::user()->id)) }}>Thuans link</a>
-        <p>Number of Visitors: 2</p>
-        <a target="_blank" href="https://mail.google.com/mail/u/0/#inbox">Email</a>
+        <p>Access the Admin Quiz Editor here: <a href={{ route("admin_quiz_editor", array("id" => Auth::user()->id)) }}>Thuans link</a></p>
+        <p>Access the feedback and enquiries inbox in the Admin email: <a target="_blank" href="https://accounts.google.com/AccountChooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F%3Fauthuser%3Dspinalcord%40gmail.com&service=mail&Email=sicure.sci@gmail.com">Email</a></p>
     </div>
 </div>
 
