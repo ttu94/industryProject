686d67fcff05548c6d43f7928783135b28addc39
ttu94 <ttu94@hotmail.com>
2017-09-11 07:09:03 +0000

    Social media accounts added to footer

--- project/app/views/includes/footer.blade.php
+++ project/app/views/includes/footer.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="col-md-4 footerDiv">
             <p class="footerFont" style="color: white">
-                Follow Us: <a target="_blank" href="https://www.facebook.com/" style="color: white">Facebook</a> | <a target="_blank" href="https://twitter.com/?lang=en" style="color: white">Twitter</i></a> | <a target="_blank" href="https://plus.google.com/" style="color: white">Google +</i></a> |
+                Follow Us: <a target="_blank" href="https://www.facebook.com/sip.cjcnscr.7" style="color: white">Facebook</a> | <a target="_blank" href="https://twitter.com/SIP_Cjcnscr" style="color: white">Twitter</i></a>
             </p>
         </div>
         <div class="col-md-4 footerDiv">
